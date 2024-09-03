b831de4bf91837c0a10a0f0a75ba0b0ea69d2d30
Winnie-Flora <contentcreatorwinnie@gmail.com>
2024-09-03 23:23:41 +0300

    List Comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
-#!/usr/bin/env python3
+# #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    return [x for x in num_list if x % 2 == 0]
+
+print(return_evens(num_list = [1,2,3,4,5,6,7,8,9,10,12]))
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + "!" for sentence in sentence_list]
+print(make_exclamation(sentence_list = ["Hello", "Time for vacation", "You look stunning"]))
